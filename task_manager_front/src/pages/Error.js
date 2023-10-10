690da1cc3d98926a158c74490a922f5f91fad69f
Othman Semlali <othmansemlali1@gmail.com>
2023-10-10 16:29:17 +0200

    update react/Error.js Component

--- task_manager_front/src/pages/Error.js
+++ task_manager_front/src/pages/Error.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f8f8f8;
+`;
+
+const ErrorMessage = styled.h1`
+  font-size: 24px;
+  color: #ff0000;
+`;
 
 function Error() {
   return (
-    <div>
-        <h1>404</h1>
-        <h2>Page not found</h2>
-        
-    </div>
-  )
+    <NotFoundContainer>
+      <ErrorMessage>404 - Page Not Found</ErrorMessage>
+      <Link to="/">Go Home</Link>
+    </NotFoundContainer>
+  );
 }
 
-export default Error
+export default Error;
